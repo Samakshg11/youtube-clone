d20e3015e28d399afa7c070751b12f14fb0efc6e
Samakshg11 <samakshgarg2005@gmail.com>
2026-04-06 23:42:43 +0530

    Polish global app shell

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,20 @@ import History from "./components/History";
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Feed />} />
-        <Route path="/search/:searchTerm" element={<SearchFeed />} />
-        <Route path="/watch/:id" element={<WatchPage />} />
-        <Route path="/history" element={<History />} />
-      </Routes>
+      <div className="app-shell">
+        <div className="app-shell__glow app-shell__glow--left" />
+        <div className="app-shell__glow app-shell__glow--right" />
+        <div className="app-shell__grid" />
+        <Navbar />
+        <div className="relative z-10">
+          <Routes>
+            <Route path="/" element={<Feed />} />
+            <Route path="/search/:searchTerm" element={<SearchFeed />} />
+            <Route path="/watch/:id" element={<WatchPage />} />
+            <Route path="/history" element={<History />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
